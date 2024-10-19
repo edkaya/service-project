ade3715547b04ecd710a2f6b1c593918c32e4821
EgeDoguKaya <egedk321@outlook.com>
2024-10-19 18:50:07 +0200

    fix

--- src/app/elektro/elektro.component.ts
+++ src/app/elektro/elektro.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatOption} from "@angular/material/autocomplete";
 import {MatSelect} from "@angular/material/select";
 import {MatSlideToggle} from "@angular/material/slide-toggle";
-import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
-import {MatTabLink, MatTabNav} from "@angular/material/tabs";
+import {ActivatedRoute, RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
+import {MatTabLink, MatTabNav, MatTabNavPanel} from "@angular/material/tabs";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-elektro',
@@ -19,14 +20,29 @@ import {MatTabLink, MatTabNav} from "@angular/material/tabs";
     RouterOutlet,
     RouterLinkActive,
     MatTabLink,
-    MatTabNav
+    MatTabNav,
+    MatTabNavPanel,
+    NgClass
   ],
   templateUrl: './elektro.component.html',
   styleUrl: './elektro.component.scss'
 })
-export class ElektroComponent {
+export class ElektroComponent implements OnInit {
   links = ['lampe', 'elek', 'it' ];
   activeLink = this.links[0];
+
+  constructor(private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    const currentPath = this.route.snapshot.firstChild?.url[0]?.path;
+    if (currentPath && this.links.includes(currentPath)) {
+      this.activeLink = currentPath;
+
+    } else {
+      this.activeLink = '';
+    }
+  }
 
   setActiveLink(link: string) {
     this.activeLink = link;
